1f7ea7e106c75f58c69b443ddfc2a44d495cd91d
DESKTOP-0MT7SCF\Nisal Suranaka <nisal444@gmail.com>
2021-01-15 23:31:18 +0530

    added new files

---
+++ js/loginscript.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+$(document).ready(function () {
+  $('.back-icon').click(function () {
+    window.history.back();
+    console.log("Clicked");
+  });
+  $('.signin-btn').click(function (e) {
+    e.preventDefault();
+    validateLoginForm();
+  });
+});
+
+function validateLoginForm() {
+  var email = $('#email').val();
+  var password = $('#password').val();
+  console.log(email);
+  var isValid = false;
+
+  $("span.error").remove();
+
+  // if (email === "") {
+  //   $('#email').after('<span style="color: red" class="error">Please enter the email!</span>');
+  // }
+  // if (password === "") {
+  //   $('#password').after('<span style="color: red" class="error">Please enter the password!</span>');
+  // }
+
+  if (email === "davidspade@gmail.com" && password === "david123") {
+    localStorage.setItem("isuserLogged", true);
+    var loc = window.location.pathname;
+    window.location.href = loc.substring(0, loc.lastIndexOf('/')) + '/index.html';
+  }
+  else {
+    if (email === "") {
+      $('#email').after('<span style="color: red" class="error">Please enter the email!</span>');
+    }
+    if (password === "") {
+      $('#password').after('<span style="color: red" class="error">Please enter the password!</span>');
+    }
+    else{
+      $('#password').after('<span style="color: red" class="error">The email and password you entered did not match our records, Please try again.</span>');
+    }
+    // $('#password').after('<span style="color: red" class="error">The email and password you entered did not match our records, Please try again.</span>');
+    localStorage.setItem("isuserLogged", false);
+  }
+}
