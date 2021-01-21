a8611849b0bc35d3bfb1d9923a7049713cac6570
Shereen Preena <shereenpreena1998@gmail.com>
2021-01-21 20:27:28 +0530

    QR Code scanner implemented

--- Product/product-ipad-script.js
+++ Product/product-ipad-script.js
@@ -3,24 +3,38 @@ $(document).ready(function () {
   
     var selectedProductIdIpad = localStorage.getItem("product-page-item");
     displayProductIpad(selectedProductIdIpad);
+   
+
+    var productListIpad = JSON.parse(data);
+    var commentListIpad = [];
+    
+    for(var k = 0; k < productListIpad.length ; k++){
+      if(productListIpad[k].id == selectedProductIdIpad){
+        commentListIpad = productListIpad[k].feedback;
+
+        localStorage.setItem("comments",JSON.stringify(commentListIpad));
+      }
+    }
+
     commentsCardIpad();
+    
 
+    $('.favme').click(function() {
+      $(this).toggleClass('active');
+      localStorage.setItem('favourites',selectedProductIdIpad);
+    });
 
-$('.favme').click(function() {
-  $(this).toggleClass('active');
-  localStorage.setItem('favourites',selectedProductIdIpad);
+    /* when a user clicks, toggle the 'is-animating' class /
+    $(".favme").on('click touchstart', function(){
+      $(this).toggleClass('is_animating');
+    });
+
+    /*when the animation is over, remove the class*/
+    $(".favme").on('animationend', function(){
+      $(this).toggleClass('is_animating');
+    });
+
 });
-
-/* when a user clicks, toggle the 'is-animating' class */
-$(".favme").on('click touchstart', function(){
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".favme").on('animationend', function(){
-  $(this).toggleClass('is_animating');
-});
-  });
   
   function displayProductIpad(productId) {
     var productsIpad = JSON.parse(data);
@@ -42,34 +56,54 @@ $(".favme").on('animationend', function(){
   
     document.getElementById("product-detail-ipad").innerHTML = output;
   }
-  
-var reviewsListIpad = JSON.parse(customer);
-var ratingValueIpad;
-var addedArrayIpad = reviewsListIpad;
 
-$(".comment-btn-ipad").click(function () {
-  var messageIpad = $("#ipad-textarea").val();
-  var testIpad = {
-    id: 4,
-    name: "David Spade",
+
+
+  
+// var reviewsListIpad = JSON.parse(data);
+var ratingValueIpad;
+// var addedArrayIpad = reviewsListIpad;
+
+// $(".comment-btn-ipad").click(function () {
+//   var messageIpad = $("#ipad-textarea").val();
+//   var testIpad = {
+//     id: 4,
+//     name: "David Spade",
+//     image: "../images/avatar.png",
+//     rating: ratingValueIpad,
+//     comment: messageIpad,
+//     date: getCurrentDateIpad()
+//   };
+//   reviewsListIpad.push(testIpad);
+//   addedArrayIpad = reviewsListIpad.reverse();
+//   commentsCardIpad();
+
+//   $("#ipad-textarea").val("");
+//   var stars = $("#stars li").parent().children("li.star");
+
+//   if(stars != null){
+//     for (i = 0; i < stars.length; i++) {
+//       $(stars[i]).removeClass("selected");
+//     }
+//   }
+// });
+
+$(".comment-btn-ipad").click(function(){
+  console.log("clicked");
+  var commentLocalListIpad = JSON.parse(localStorage.getItem("comments"));
+  var userCommentIpad = $("#ipad-textarea").val();
+  var commentObj = {
+    username : "David Spade",
     image: "../images/avatar.png",
     rating: ratingValueIpad,
-    comment: messageIpad,
+    comment: userCommentIpad,
     date: getCurrentDateIpad()
-  };
-  reviewsListIpad.push(testIpad);
-  addedArrayIpad = reviewsListIpad.reverse();
-  commentsCardIpad();
-
-  $("#ipad-textarea").val("");
-  var stars = $("#stars li").parent().children("li.star");
-
-  if(stars != null){
-    for (i = 0; i < stars.length; i++) {
-      $(stars[i]).removeClass("selected");
-    }
   }
-});
+  commentLocalListIpad.push(commentObj);
+  console.log(commentLocalListIpad);
+  localStorage.setItem("comments", JSON.stringify(commentLocalListIpad));
+  commentsCardIpad();
+})
 
 $("#stars li").on("click", function () {
   var onStar = parseInt($(this).data("value"), 10);
@@ -99,29 +133,49 @@ $.fn.stars = function () {
   });
 };
 
-function commentsCardIpad() {
-  var reviewIpad = "";
+// function commentsCardIpad() {
+//   var reviewIpad = "";
 
-  if (addedArrayIpad.length != []) {
-    for (var i = 0; i < addedArrayIpad.length; i++) {
-      $(function () {
+//   if (addedArrayIpad.length != []) {
+//     for (var i = 0; i < addedArrayIpad.length; i++) {
+//       $(function () {
+//         $(".stars").stars();
+//       });
+
+//       reviewIpad += '<li class="comment-details-ipad"><div class="product-detail-section-ipad">';
+//       reviewIpad += '<div class="avatar-ipad"><img class="user-avatar-ipad" src="' + addedArrayIpad[i].image + '" alt = "profile-picture">';
+//       reviewIpad += '<div class="name-ipad"><span class="user-name-ipad">' + addedArrayIpad[i].name + '</span>';
+//       reviewIpad += '<div class="date-ipad"><span class="stars" data-rating="' + addedArrayIpad[i].rating + '" data-num-stars="5" ></span>Posted on ' + addedArrayIpad[i].date + ' </div><br><p class="user-comment-ipad">"' + addedArrayIpad[i].comment + '"</p> </div></div></div></li><br>';
+//     }
+//   }
+//   document.getElementById("review-card-ipad").innerHTML = reviewIpad;
+// }
+
+function commentsCardIpad(){
+  var reviewIpad = "";
+  var commentsIpad = JSON.parse(localStorage.getItem("comments")).reverse();
+
+    for( var m = 0; m <commentsIpad.length ; m++){
+      console.log(commentsIpad[m].username);
+      $(function() {
         $(".stars").stars();
-      });
+      })
 
       reviewIpad += '<li class="comment-details-ipad"><div class="product-detail-section-ipad">';
-      reviewIpad += '<div class="avatar-ipad"><img class="user-avatar-ipad" src="' + addedArrayIpad[i].image + '" alt = "profile-picture">';
-      reviewIpad += '<div class="name-ipad"><span class="user-name-ipad">' + addedArrayIpad[i].name + '</span>';
-      reviewIpad += '<div class="date-ipad"><span class="stars" data-rating="' + addedArrayIpad[i].rating + '" data-num-stars="5" ></span>Posted on ' + addedArrayIpad[i].date + ' </div><br><p class="user-comment-ipad">"' + addedArrayIpad[i].comment + '"</p> </div></div></div></li><br>';
+      reviewIpad += '<div class="avatar-ipad"><img class="user-avatar-ipad" src="' + commentsIpad[m].image + '" alt = "profile-picture">';
+      reviewIpad += '<div class="name-ipad"><span class="user-name-ipad">' + commentsIpad[m].username + '</span>';
+      reviewIpad += '<div class="date-ipad"><span class="stars" data-rating="' + commentsIpad[m].rating + '" data-num-stars="5" ></span>Posted on ' + commentsIpad[m].date + ' </div><br><p class="user-comment-ipad">"' + commentsIpad[m].comment + '"</p> </div></div></div></li><br>';
+
     }
-  }
-  document.getElementById("review-card-ipad").innerHTML = reviewIpad;
+
+    document.getElementById("review-card-ipad").innerHTML = reviewIpad;
 }
 
 function getCurrentDateIpad(){
-    today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; 
-    var yyyy = today.getFullYear();
+    todayIpad = new Date();
+    var dd = todayIpad.getDate();
+    var mm = todayIpad.getMonth()+1; 
+    var yyyy = todayIpad.getFullYear();
   
     if(dd<10) dd='0'+dd;
     if(mm<10) mm='0'+mm;  
