0d8612b9b9d0394fdb7ec77ee57d98d378f0cd9e
Senuri Ranasinghe <senurikaveesha1998@gmail.com>
2021-01-22 02:38:24 +0530

    Product changes

--- Product/product-ipad-script.js
+++ Product/product-ipad-script.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    // localStorage.setItem("product", "product1");
-  
     var selectedProductIdIpad = localStorage.getItem("product-page-item");
     displayProductIpad(selectedProductIdIpad);
    
@@ -24,10 +22,6 @@ $(document).ready(function () {
       localStorage.setItem('favourites',selectedProductIdIpad);
     });
 
-    /* when a user clicks, toggle the 'is-animating' class /
-    $(".favme").on('click touchstart', function(){
-      $(this).toggleClass('is_animating');
-    });
 
     /*when the animation is over, remove the class*/
     $(".favme").on('animationend', function(){
@@ -57,36 +51,8 @@ $(document).ready(function () {
     document.getElementById("product-detail-ipad").innerHTML = output;
   }
 
-
-
-  
-// var reviewsListIpad = JSON.parse(data);
 var ratingValueIpad;
-// var addedArrayIpad = reviewsListIpad;
 
-// $(".comment-btn-ipad").click(function () {
-//   var messageIpad = $("#ipad-textarea").val();
-//   var testIpad = {
-//     id: 4,
-//     name: "David Spade",
-//     image: "../images/avatar.png",
-//     rating: ratingValueIpad,
-//     comment: messageIpad,
-//     date: getCurrentDateIpad()
-//   };
-//   reviewsListIpad.push(testIpad);
-//   addedArrayIpad = reviewsListIpad.reverse();
-//   commentsCardIpad();
-
-//   $("#ipad-textarea").val("");
-//   var stars = $("#stars li").parent().children("li.star");
-
-//   if(stars != null){
-//     for (i = 0; i < stars.length; i++) {
-//       $(stars[i]).removeClass("selected");
-//     }
-//   }
-// });
 
 $(".comment-btn-ipad").click(function(){
   console.log("clicked");
@@ -133,30 +99,12 @@ $.fn.stars = function () {
   });
 };
 
-// function commentsCardIpad() {
-//   var reviewIpad = "";
-
-//   if (addedArrayIpad.length != []) {
-//     for (var i = 0; i < addedArrayIpad.length; i++) {
-//       $(function () {
-//         $(".stars").stars();
-//       });
-
-//       reviewIpad += '<li class="comment-details-ipad"><div class="product-detail-section-ipad">';
-//       reviewIpad += '<div class="avatar-ipad"><img class="user-avatar-ipad" src="' + addedArrayIpad[i].image + '" alt = "profile-picture">';
-//       reviewIpad += '<div class="name-ipad"><span class="user-name-ipad">' + addedArrayIpad[i].name + '</span>';
-//       reviewIpad += '<div class="date-ipad"><span class="stars" data-rating="' + addedArrayIpad[i].rating + '" data-num-stars="5" ></span>Posted on ' + addedArrayIpad[i].date + ' </div><br><p class="user-comment-ipad">"' + addedArrayIpad[i].comment + '"</p> </div></div></div></li><br>';
-//     }
-//   }
-//   document.getElementById("review-card-ipad").innerHTML = reviewIpad;
-// }
 
 function commentsCardIpad(){
   var reviewIpad = "";
   var commentsIpad = JSON.parse(localStorage.getItem("comments")).reverse();
 
     for( var m = 0; m <commentsIpad.length ; m++){
-      console.log(commentsIpad[m].username);
       $(function() {
         $(".stars").stars();
       })
@@ -164,7 +112,7 @@ function commentsCardIpad(){
       reviewIpad += '<li class="comment-details-ipad"><div class="product-detail-section-ipad">';
       reviewIpad += '<div class="avatar-ipad"><img class="user-avatar-ipad" src="' + commentsIpad[m].image + '" alt = "profile-picture">';
       reviewIpad += '<div class="name-ipad"><span class="user-name-ipad">' + commentsIpad[m].username + '</span>';
-      reviewIpad += '<div class="date-ipad"><span class="stars" data-rating="' + commentsIpad[m].rating + '" data-num-stars="5" ></span>Posted on ' + commentsIpad[m].date + ' </div><br><p class="user-comment-ipad">"' + commentsIpad[m].comment + '"</p> </div></div></div></li><br>';
+      reviewIpad += '<div class="date-ipad"><span class="stars" data-rating="' + commentsIpad[m].rating + '" data-num-stars="5" ></span>Posted on ' + commentsIpad[m].date + ' </div><p class="user-comment-ipad">"' + commentsIpad[m].comment + '"</p> </div></div></div></li><br>';
 
     }
 
